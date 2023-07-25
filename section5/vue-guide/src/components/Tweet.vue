aaeb77d20beeba2016a48ed139bb4d382b215df4
Tomo <t.fujinawa@miracleave.co.jp>
2023-07-25 15:07:27 +0900

    静的なリストまで作業

--- section5/vue-guide/src/components/Tweet.vue
+++ section5/vue-guide/src/components/Tweet.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
+const tweets = [
+  {id: 0, description: 'Hello, World!'},
+  {id: 1, description: 'This is the second tweeet.'}
+]
+
 </script>
 
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input/>
+      <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+      <ul>
+        <!-- v-forディレクティブ -->
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -16,10 +35,32 @@
   display: flex;
   flex-direction: column;
   align-items: center;
+  row-gap: 20px;
   background-color: aliceblue;
   padding: 24px 0;
   width: 60%;
   margin-bottom: 12px;
   border-radius: 4px;
 }
+
+.tweet-list {
+  list-style: none;
+}
+
+button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border: none;
+  border-radius: 2px;
+  width: 60px;
+  height: 22px;
+  cursor: pointer;
+  transition: filter .25s;
+}
+
+button:hover {
+  filter: brightness(85%) contrast(130%);
+}
+
 </style>
