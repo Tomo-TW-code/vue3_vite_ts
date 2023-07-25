e292507e8363adf42c45e78cf8fd31eba8e6f1e5
Tomo <t.fujinawa@miracleave.co.jp>
2023-07-26 08:14:36 +0900

    slotまで作業

---
+++ section8/8_2/vue-guide/src/components/Card.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps<{
+  title: string,
+}>();
+</script>
+
+<template>
+  <div class="card-container">
+    <div class="content">
+      <span class="title">{{ title }}</span>
+      <slot></slot>
+    </div>
+    <div class="footer">
+      <slot name="footer">
+        <span>Default Footer</span>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-container {
+  box-shadow: 2, 8px, black;
+  border-radius: 8px;
+  padding: 20px 0 0 0;
+  width: 300px;
+  background-color: #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.content {
+  padding: 0 20px;
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 600;
+}
+
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  background-color: rgb(22, 119, 203);
+  width: 100%;
+  border-radius: 0 0 8px 8px;
+}
+
+</style>
